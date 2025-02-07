ca32f694023f1def5697056deb847f2bc3caa80d
Marlon-Tumamak <tumamak.mariomarlon@gmail.com>
2025-02-07 09:30:23 +0800

    Added endpoint for records per student

--- CITCStudentAttendance/attendance/urls.py
+++ CITCStudentAttendance/attendance/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ListStudents, StudentsDetail, ListRecords, RecordsDetail
+from .views import ListStudents, StudentsDetail, ListRecords, RecordsDetail, UserRecordsList
 
 urlpatterns = [
     path('students/', ListStudents.as_view(), name='students-list'),
     path('students/<int:pk>/', StudentsDetail.as_view(), name='students-detail'),
     path('records/', ListRecords.as_view(), name='records-list'),
     path('records/<int:pk>/', RecordsDetail.as_view(), name='records-detail'),
+    path('student/records/<int:user_id>/', UserRecordsList.as_view(), name='user-records-list'),
 ]
--- CITCStudentAttendance/attendance/views.py
+++ CITCStudentAttendance/attendance/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .serializers import UserSerializer, RecordSerializer
 from .models import User, Record
 
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 class ListStudents(ListCreateAPIView):
     queryset = User.objects.all()
@@ -20,3 +20,9 @@ class RecordsDetail(RetrieveUpdateDestroyAPIView):
     queryset = Record.objects.all()
     serializer_class = RecordSerializer    
 
+class UserRecordsList(ListAPIView):
+    serializer_class = RecordSerializer
+
+    def get_queryset(self):
+        user_id = self.kwargs['user_id']
+        return Record.objects.filter(user_id=user_id)
