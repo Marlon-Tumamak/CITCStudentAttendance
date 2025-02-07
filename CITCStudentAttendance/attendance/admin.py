8918e753615f20a62c19972ae96e38d46ab6ea78
Marlon-Tumamak <tumamak.mariomarlon@gmail.com>
2025-02-07 14:20:41 +0800

    corseheaders

--- CITCStudentAttendance/attendance/admin.py
+++ CITCStudentAttendance/attendance/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import User, Record, Date
+from .models import User, Record
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Record)
-admin.site.register(Date)
