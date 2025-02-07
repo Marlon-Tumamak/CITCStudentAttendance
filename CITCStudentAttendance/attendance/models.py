2f7e2e90b64fceb1684705b21249186636b4b671
Marlon-Tumamak <tumamak.mariomarlon@gmail.com>
2025-02-07 10:35:59 +0800

    Added description and date foriegn key to class model records

--- CITCStudentAttendance/attendance/models.py
+++ CITCStudentAttendance/attendance/models.py
@@ -9,11 +9,18 @@ class User(models.Model):
     def __str__(self):
         return self.firstName + " " + self.lastName
     
+class Date(models.Model):
+    date = models.DateField()
+
+    def __str__(self):
+        return str(self.date)
+    
 class Record(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    date = models.DateField()
+    date = models.ForeignKey(Date, on_delete=models.CASCADE)
     timeIn = models.TimeField()
     timeOut = models.TimeField()
+    description = models.TextField(max_length=200)
 
     def __str__(self):
         return self.user.firstName + " " + self.user.lastName + " " + str(self.date)    
