95501ac015cb9135c2a8965d6de9554653b576a5
Marlon-Tumamak <tumamak.mariomarlon@gmail.com>
2025-02-07 15:26:38 +0800

    change model for changing ideas

--- CITCStudentAttendance/attendance/admin.py
+++ CITCStudentAttendance/attendance/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import User, Record
+from .models import User, Record, Mode
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Record)
+admin.site.register(Mode)
+
